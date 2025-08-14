89f29ac12d9998eae329ac8bf3e291c33d33b2b8
Toni Barth <software@satoprogs.de>
2025-08-14 14:14:32 +0200

    add users edit command to hitster-cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -23,7 +23,7 @@ enum Commands {
         /// the path to the csv file
         file: PathBuf,
     },
-    /// manage user permissions
+    /// manage users
     Users(UsersArgs),
 }
 
@@ -38,8 +38,24 @@ struct UsersArgs {
 
 #[derive(Subcommand)]
 enum UsersCommands {
+    /// edit a user
+    Edit(UsersEditArgs),
     /// list all users currently in the database
     List {},
+}
+
+#[derive(Args)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(arg_required_else_help = true)]
+struct UsersEditArgs {
+    /// the user id (see hitster-cli users list)
+    id: String,
+    /// make the user an admin (grand them all permissions)
+    #[arg(short, long, group = "perm", action = clap::ArgAction::SetTrue)]
+    admin: Option<bool>,
+    /// give the user specific permissions (see hitster-cli users list)
+    #[arg(short, long, group = "perm")]
+    permissions: Option<u32>,
 }
 
 #[tokio::main]
@@ -56,7 +72,21 @@ async fn main() -> Result<ExitCode, Box<dyn Error>> {
         }
         Commands::Users(args) => {
             let db = env::var("DATABASE_URL").expect("DATABASEURL environment variable not found");
-            match args.command {
+            match &args.command {
+                UsersCommands::Edit(args) => {
+                    let success = users::edit(
+                        &db,
+                        &args.id,
+                        users::EditArgs {
+                            admin: args.admin.unwrap_or(false),
+                            permissions: args.permissions.unwrap_or(0),
+                        },
+                    )
+                    .await;
+                    if !success {
+                        return Ok(ExitCode::from(1));
+                    }
+                }
                 UsersCommands::List {} => {
                     let success = users::list(&db).await;
                     if !success {
--- cli/src/users.rs
+++ cli/src/users.rs
@@ -1,5 +1,11 @@
+use bitflags::Flags;
 use hitster_core::{Permissions, User};
 use sqlx::sqlite::SqlitePool;
+
+pub struct EditArgs {
+    pub admin: bool,
+    pub permissions: u32,
+}
 
 pub async fn list(url: &str) -> bool {
     if let Ok(pool) = SqlitePool::connect(url).await {
@@ -14,13 +20,76 @@ pub async fn list(url: &str) -> bool {
             println!("\tid: {}", user.id);
             println!("\tpermissions:");
             for flag in Permissions::all().iter_names() {
-                println!("\t\t{}: {}", flag.0.to_lowercase(), user.permissions.contains(flag.1));
+                println!(
+                    "\t\t{} ({}): {}",
+                    flag.0.to_lowercase(),
+                    flag.1.bits(),
+                    user.permissions.contains(flag.1)
+                );
             }
         }
 
         true
     } else {
-        println!("unable to open {}", url);
+        println!("unable to open {url}");
+        false
+    }
+}
+
+pub async fn edit(url: &str, id: &str, args: EditArgs) -> bool {
+    let permissions = if args.admin {
+        Some(Permissions::all())
+    } else {
+        let p = Permissions::from_bits_retain(args.permissions);
+        if p.contains_unknown_bits() {
+            println!(
+                "unknown permission bits: {}",
+                p.bits() & !Permissions::all().bits()
+            );
+            None
+        } else {
+            Some(p)
+        }
+    };
+
+    if permissions.is_none() {
+        return false;
+    }
+
+    if let Ok(pool) = SqlitePool::connect(url).await {
+        let mut conn = pool.acquire().await.unwrap();
+        if let Some(user) = sqlx::query_as::<_, User>("SELECT * FROM users WHERE id = ?")
+            .bind(id)
+            .fetch_optional(&mut *conn)
+            .await
+            .unwrap()
+        {
+            let _ = sqlx::query("UPDATE users SET permissions = ? WHERE id = ?")
+                .bind(permissions.as_ref().unwrap().bits())
+                .bind(id)
+                .execute(&mut *conn)
+                .await;
+
+            println!(
+                "permissions for user {} ({}) were set to:",
+                user.name, user.id
+            );
+
+            for flag in Permissions::all().iter_names() {
+                println!(
+                    "\t{} ({}): {}",
+                    flag.0.to_lowercase(),
+                    flag.1.bits(),
+                    permissions.as_ref().unwrap().contains(flag.1)
+                );
+            }
+            true
+        } else {
+            println!("no user with that id was found in the database");
+            false
+        }
+    } else {
+        println!("unable to open {url}");
         false
     }
 }
